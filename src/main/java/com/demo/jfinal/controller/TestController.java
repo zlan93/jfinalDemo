8b2ade8c5a4c4d28198af438a874319d1dc401d2
zlan93 <zhanglan56226710>
2017-10-19 08:38:49 +0800

    注释

--- src/main/java/com/demo/jfinal/controller/TestController.java
+++ src/main/java/com/demo/jfinal/controller/TestController.java
@@ -6,13 +6,17 @@ import com.jfinal.plugin.activerecord.Record;
 
 import java.util.List;
 
+//config配置方式
 public class TestController extends Controller {
-
-    //config配置方式
+//    Action 方法必须在 Controller 中声明，该方法必须是 public 可见且没有形参。
+    
+    //默认方法
     public void index() {
         String name = getPara("name");
         setAttr("blogs", name);
 //        Po p = getBean(Po.class, "p");
+//        getModel()需要表单域名称对应于数据表字段名，而 getBean()则依赖于 setter 方法，表单域名对应于 setter 方法去掉”set”前缀字符后剩下的字符串字母变小写
+
 //        renderText("Hello Maven Jfinal");//直接输出文字
 
         //查询数据库数据
--- src/main/java/com/demo/jfinal/util/TestConfig.java
+++ src/main/java/com/demo/jfinal/util/TestConfig.java
@@ -2,7 +2,7 @@ package com.demo.jfinal.util;
 
 import com.demo.jfinal.controller.LoginController;
 import com.demo.jfinal.controller.TestController;
-import com.demo.jfinal.model.db.*;
+import com.demo.jfinal.model.db._MappingKit;
 import com.demo.jfinal.shiro.ShiroInterceptor;
 import com.demo.jfinal.shiro.ShiroPlugin;
 import com.jfinal.config.*;
@@ -25,8 +25,9 @@ public class TestConfig extends JFinalConfig {
      */
     @Override
     public void configConstant(Constants constants) {
+        Prop p = PropKit.use("SystemConfig.txt");//PropKit 工具类用来操作外部配置文件
         // 开发者模式，正式部署时关闭
-        constants.setDevMode(true);
+        constants.setDevMode(p.getBoolean("devMode"));
         constants.setBaseViewPath("/WEB-INF/views");//配置页面访问主路径,webapp下
 
         constants.setViewType(ViewType.JSP);//设置视图类型为Jsp，否则默认为FreeMarker
@@ -40,7 +41,7 @@ public class TestConfig extends JFinalConfig {
     @Override
     public void configRoute(Routes me) {
         this.routes = me;
-        me.add("/test", TestController.class, "/");//路由，指定调用哪个controller,若不添加第三个参数访问时会默认添加/test路径
+        me.add("/test", TestController.class, "/");//路由，指定调用哪个controller,访问地址是/controllerKey/viewPath/方法名（index()是默认方法，不需要加方法名）
         me.add("/", LoginController.class, "/");
     }
 
@@ -86,29 +87,6 @@ public class TestConfig extends JFinalConfig {
         // 方式二：配置数据表映射写到一个文件中
         _MappingKit.mapping(arp);
 
-        //视图mapping
-//        arp.addMapping("CV_CURRENT_PROCESS", CvCurrentProcess.class);
-//        arp.addMapping("CV_DAILY_TOTAL_MILEAGE", CvDailyTotalMileage.class);
-//        arp.addMapping("CV_ENT_MILEAGE", CvEntMileage.class);
-//        arp.addMapping("CV_ENT_VEHICLE_COUNT", CvEntVehicleCount.class);
-//        arp.addMapping("CV_ENT_VEHICLE_SIM_COUNT", CvEntVehicleSimCount.class);
-//        arp.addMapping("CV_GREEN_VIEW", CvGreenView.class);
-//        arp.addMapping("CV_GREEN_VIEW_copy", CvGreenViewCopy.class);
-//        arp.addMapping("CV_MONTH_DRIVER_COUNT", CvMonthDriverCount.class);
-//        arp.addMapping("CV_MONTH_MILEAGE_COUNT", CvMonthMileageCount.class);
-//        arp.addMapping("CV_MONTH_SPEED_AVERAGE", CvMonthSpeedAverage.class);
-//        arp.addMapping("CV_MONTH_VEHICLE_COUNT", CvMonthVehicleCount.class);
-//        arp.addMapping("CV_MONTH_VEHICLE_TIMES_COUNT", CvMonthVehicleTimesCount.class);
-//        arp.addMapping("CV_ORDER_MONITOR", CvOrderMonitor.class);
-//        arp.addMapping("CV_REPORT_AREA_ENT_COUNT", CvReportAreaEntCount.class);
-//        arp.addMapping("CV_REPORT_OFFLINE_COUNT", CvReportOfflineCount.class);
-//        arp.addMapping("CV_REPORT_ROAD_COUNT", CvReportRoadCount.class);
-//        arp.addMapping("CV_REPORT_VEHICLE_ONLINE_COUNT", CvReportVehicleOnlineCount.class);
-//        arp.addMapping("CV_VEHICLE_INSURE", CvVehicleInsure.class);
-//        arp.addMapping("CV_VEHICLE_MONITOR", CvVehicleMonitor.class);
-//        arp.addMapping("CV_VEHICLE_PASS_LICENSE", CvVehiclePassLicense.class);
-//        arp.addMapping("CV_VEHICLE_TRACK", CvVehicleTrack.class);
-//        arp.addMapping("CV_VEHICLE_TREE", CvVehicleTree.class);
         me.add(c3p0Plugin);
         me.add(arp);
 
